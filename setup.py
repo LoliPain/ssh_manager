7af79b463ba05228dbcfb4d60fb9ec62b0f04320
LoliPain <p2love.loli@gmail.com>
2023-03-29 19:37:55 +0400

    setuptools

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='ssh_manager',
+    version='0.1',
+    packages=['ssh_manager', 'ssh_manager.stored'],
+    install_requires=[
+        'simple_term_menu',
+    ],
+    entry_points={
+        'console_scripts':
+            [
+                'ssh_m = ssh_manager.main:main'
+            ]
+    },
+)
