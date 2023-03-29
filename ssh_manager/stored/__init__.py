20b645a2aa785c7884081cfd09df19a8b36282c2
LoliPain <p2love.loli@gmail.com>
2023-03-29 23:28:26 +0400

    current shellscript

--- ssh_manager/stored/__init__.py
+++
@@ -1,4 +0,0 @@
-from .append import append_to_stored
-from .read import proceed_stored
-
-__all__ = ["proceed_stored", "append_to_stored"]
--- ssh_manager/stored/store_path.py
+++
@@ -1,3 +0,0 @@
-from pathlib import Path
-
-store_path = f'{Path(__file__).parent.resolve()}/cxs.json'  # Path to storage file
