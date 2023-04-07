7c28abbae94b338fda4723c803f03fbae0b9b8c1
LoliPain <p2love.loli@gmail.com>
2023-04-08 02:30:15 +0400

    setup.py update

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup
 
 setup(
     name='ssh_manager',
-    python_requires='>=3.10',
     version='0.1.1',
+    url='https://github.com/LoliPain/ssh_manager',
+    python_requires='>=3.10',
     packages=['ssh_manager', 'ssh_manager.stored'],
     install_requires=[
         'simple_term_menu',
