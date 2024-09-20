5e73ccfbad1894a9df133a870d34a36c30b548a0
LoliPain <x@lolipa.in>
2024-09-20 07:53:43 +0400

    process creating directly from menu

--- ssh_manager/shell.py
+++ ssh_manager/shell.py
@@ -1,16 +1,17 @@
 import os
 import sys
+from typing import Optional
 
 from InquirerPy import inquirer, get_style
-from InquirerPy.base.control import Choice
 from InquirerPy.enum import INQUIRERPY_POINTER_SEQUENCE as pointer_code
+from InquirerPy.base.control import Choice
 
 from .connection import Connection
-from .stored import proceed_stored
+from .stored import proceed_stored, append_to_stored
 from .tmux import run_in_tmux
 
 
-def one_time_selection() -> Connection:
+def one_time_selection() -> Optional[Connection]:
     """Start an interactive selection
 
     :return: Selected connection instance
@@ -25,14 +26,30 @@ def one_time_selection() -> Connection:
         mandatory=False,
         show_cursor=False,
         raise_keyboard_interrupt=False,
-        long_instruction="exit: C-c, q",
+        long_instruction="new: n, delete: d\nexit: C-c, q",
         keybindings={"skip": [{"key": "q"}, {"key": "c-c"}]},
         choices=[Choice(value=i, name=str(_)) for i, _ in enumerate(store)],
         style=get_style({"answermark": "#61afef"}, style_override=False)
-    ).execute()
-    if selected is None:
-        sys.exit(0)  # Exit on 'q' press
-    return store[selected]
+    )
+
+    @selected.register_kb('d')
+    def _delete_entry(ctx):
+        ctx.app.exit(result='del')
+
+    @selected.register_kb('n')
+    def _new_entry(ctx):
+        ctx.app.exit(result='new')
+
+    selected = selected.execute()
+    match selected:
+        case 'new':
+            return append_to_stored(new_stored_entry())
+        case 'del':
+            ...
+        case None:
+            sys.exit(0)  # Exit on 'skip' action
+        case _:
+            return store[selected]
 
 
 def new_stored_entry() -> Connection:
@@ -68,6 +85,8 @@ def open_ssh() -> None:
     :return: No.
     """
     connection = one_time_selection()
+    if not connection:
+        open_ssh()
     if os.environ.get(connection.env_passwd()):
         if os.environ.get("TMUX"):
             run_in_tmux(connection)
