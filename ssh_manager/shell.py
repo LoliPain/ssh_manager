a64a2d1fa2d53a0f89bd7837160b4b71dd86f4dc
LoliPain <x@lolipa.in>
2024-09-25 06:52:16 +0400

    dont overwrite interrupts due to handling

--- ssh_manager/shell.py
+++ ssh_manager/shell.py
@@ -31,11 +31,9 @@ def one_time_selection() -> Optional[Connection]:
         amark=POINTER_CODE,
         cycle=True,
         vi_mode=True,
-        mandatory=False,
         show_cursor=False,
-        raise_keyboard_interrupt=False,
         long_instruction="new: n, delete: d\nexit: C-c, q",
-        keybindings={"skip": [{"key": "q"}, {"key": "c-c"}]},
+        keybindings={"interrupt": [{"key": "q"}, {"key": "c-c"}]},
         choices=[Choice(value=(_MenuAction.Select, i), name=str(_)) for i, _ in enumerate(store)],
         style=get_style({"answermark": "#61afef"}, style_override=False)
     )
@@ -52,9 +50,7 @@ def one_time_selection() -> Optional[Connection]:
         """
         ctx.app.exit(result=(_MenuAction.New,))
 
-    selected: Optional[Tuple[_MenuAction, int]] = menu.execute()
-    if not selected:
-        raise SystemExit()  # Exit on 'skip' action
+    selected: Tuple[_MenuAction, int] = menu.execute()
     match selected[0]:
         case _MenuAction.New:
             append_to_stored(new_stored_entry())
@@ -86,7 +82,6 @@ def new_stored_entry() -> Connection:
         """
         return inquirer.text(
             message=message,
-            mandatory=True,
             amark=POINTER_CODE,
             validate=lambda self: len(self) > 0,
             long_instruction="exit: C-c",
